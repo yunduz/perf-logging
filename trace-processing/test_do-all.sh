6e727c45add171a363b27e0ad13b916a8fdde692
Yunduz <yunduz@gmail.com>
2016-10-14 14:33:39 -0700

    commented out some lines, work in progress

--- trace-processing/test_do-all.sh
+++ trace-processing/test_do-all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-BUILD_DIR=${HOME}/dinamite/llvm-3.5.0.src/projects/dinamite
+#BUILD_DIR=${HOME}/dinamite/llvm-3.5.0.src/projects/dinamite
+BUILD_DIR=${HOME}/repos/wiredtiger_instr_test
 
 if [ -z ${SCRIPTS_HOME} ]; then
     SCRIPTS_HOME=${HOME}/repos/perf-logging/trace-processing
@@ -9,11 +10,12 @@ fi
 for file in "$@";
 do
     echo Working on file ${file}
-    ${HOME}/datamancers-bintrace-toolkit-4941d75a26e4/trace_parser -p print -a ac_short -m ${BUILD_DIR} ${file} | ${SCRIPTS_HOME}/process-logs.py --prefix ${file} > ${file}.txt.summary
-    ${SCRIPTS_HOME}/timing_to_synoptic.py ${file}.txt
-    ${SCRIPTS_HOME}/run-synoptic.sh ${file}.txt.synoptic
-    ${SCRIPTS_HOME}/prettify-dotfiles.py ${file}.txt
-    dot -Tpng ${file}.txt.synoptic-enhanced.dot > ${file}.png
+    ${HOME}/datamancers-bintrace-toolkit-4941d75a26e4/trace_parser -p print -m ${BUILD_DIR} ${file}
+    # ${HOME}/datamancers-bintrace-toolkit-4941d75a26e4/trace_parser -p print -a ac_short -m ${BUILD_DIR} ${file} | ${SCRIPTS_HOME}/process-logs.py --prefix ${file} > ${file}.txt.summary
+    # ${SCRIPTS_HOME}/timing_to_synoptic.py ${file}.txt
+    # ${SCRIPTS_HOME}/run-synoptic.sh ${file}.txt.synoptic
+    # ${SCRIPTS_HOME}/prettify-dotfiles.py ${file}.txt
+    # dot -Tpng ${file}.txt.synoptic-enhanced.dot > ${file}.png
 done
 
 rm *.synoptic.condensed*
